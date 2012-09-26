4cec33db317bd6930184ab12f075d533f6d83cd2
vegoshin <vegoshin@digt.ru>
2012-09-26 14:33:28 +0400

    Remove old sources

--- server/yate/windows/run-console.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd ..
-windows\Release\yate-console -n yate-console -m windows\Release -c conf.d -e share -vvvvv -Dato
-cd windows
--- server/yate/windows/run-qt4.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd ..
-windows\Release\yate-qt4 -n yate-qt4 -m windows\Release -c conf.d -e share -vvv
-cd windows
