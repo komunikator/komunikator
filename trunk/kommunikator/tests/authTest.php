b6d0625b8cd41a91b6d9856baf9e6a453f1144a2
alexander.elkin <1@172.17.2.22>
2013-03-14 17:42:52 +0300

    Добавлен демо тест config.php

--- trunk/kommunikator/tests/authTest.php
+++ trunk/kommunikator/tests/authTest.php
@@ -1,6 +1,6 @@
 <?php
-require_once dirname(__FILE__) . '\..\src\php\util.php';
-require_once dirname(__FILE__) . '\..\src\php\action\auth.php';
+require_once dirname(__FILE__) . '/../src/php/util.php';
+require_once dirname(__FILE__) . '/../src/php/action/auth.php';
 /*
 class authTest extends PHPUnit_Framework_TestCase {
 
