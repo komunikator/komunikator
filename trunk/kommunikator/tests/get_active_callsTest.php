4996829f7c5dbd0ea2d243db55dee332ffbbf730
ale <>
2013-03-22 10:31:54 +0300

    Добавлен config для Test Suite Добавлен тест get_active_calls

---
+++ trunk/kommunikator/tests/get_active_callsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class get_active_callsTest extends PHPUnit_Framework_TestCase {
+
+    public function testActiveCalls() {
+        $params = array('action' => 'get_active_calls', 'session' => file_get_contents('session'));
+        $out = shell_exec("php data_.php " . addslashes(json_encode($params)));
+        $res = json_decode($out);
+        $this->assertEquals(true, $res->success);
+    }
+
+}
+
+?>
