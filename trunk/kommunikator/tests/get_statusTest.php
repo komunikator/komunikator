d2ed92f40a294b39e4c334571ac412f3f36c8fcc
ale <>
2013-03-22 13:59:16 +0300

    Обновлен механизм передачи сессии в тестах

--- trunk/kommunikator/tests/get_statusTest.php
+++ trunk/kommunikator/tests/get_statusTest.php
@@ -5,7 +5,7 @@ class get_statusTest extends PHPUnit_Framework_TestCase {
     public static function provider() {
         return array(
             array('c3h8rud74rfcap9bpae40e2pu5', false), //invalid session id
-            array(file_get_contents(sys_get_temp_dir().'/session'), true),
+            array('valid_session', true)
         );
     }
 
@@ -13,6 +13,8 @@ class get_statusTest extends PHPUnit_Framework_TestCase {
      * @dataProvider provider
      */
     public function testStatus($session, $result) {
+        if ($session == 'valid_session')
+            $session = file_get_contents(sys_get_temp_dir() . '/session');
         $params = array('action' => 'get_status', 'session' => $session);
         $out = shell_exec("php data_.php " . addslashes(json_encode($params)));
         $res = json_decode($out);
--- trunk/kommunikator/tests/valid_actionTest.php
+++ trunk/kommunikator/tests/valid_actionTest.php
@@ -5,7 +5,7 @@ class valid_actionTest extends PHPUnit_Framework_TestCase {
     public static function provider() {
         return array(
             array('invalid_action', false), //invalid action name
-            array('get_status', true),
+            array('get_status', true)
         );
     }
 
@@ -13,7 +13,7 @@ class valid_actionTest extends PHPUnit_Framework_TestCase {
      * @dataProvider provider
      */
     public function testValidAction($action, $result) {
-        $params = array('session' => file_get_contents(sys_get_temp_dir().'/session'), 'action' => $action);
+        $params = array('session' => file_get_contents(sys_get_temp_dir() . '/session'), 'action' => $action);
         $out = shell_exec("php data_.php " . addslashes(json_encode($params)));
         $res = json_decode($out);
         $this->assertEquals($result, $res->success);
