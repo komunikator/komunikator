25f1a89417b9038e2bfe3fc4531e6c8f4efac29a
ale <>
2013-03-28 10:59:38 +0300

    обновлен update_dids

--- trunk/kommunikator/src/php/action/update_dids.php
+++ trunk/kommunikator/src/php/action/update_dids.php
@@ -1,4 +1,5 @@
 <?
+
 need_user();
 
 $values = array();
@@ -6,29 +7,32 @@ $data = json_decode($HTTP_RAW_POST_DATA);
 $rows = array();
 $values = array();
 
-if ($data && !is_array($data)) $data = array($data);
+if ($data && !is_array($data))
+    $data = array($data);
 foreach ($data as $row) {
     $values = array();
-    foreach ($row as $key=>$value)
+    foreach ($row as $key => $value)
         switch ($key) {
             case 'did':
-		break;	
-            case 'default_dest':
-            case 'destination':
-                if (preg_match('/\d{3}/',$value)) {
-                    $values['extension_id']= " (select extension_id from extensions where extension = '$value') "; 
-                    $values['group_id']= 'null'; 
-                }
-                else {
-                    $values['group_id']= " (select group_id from groups where groups.group = '$value') "; 
-                    $values['extension_id']= 'null'; 
-                }	
                 break;
-            //case 'destination':
-            //    $values[$key]="'$source[$value]'"; 
-            //    break;
-            default:	
-                $values[$key]="'$value'"; 
+            case 'default_dest':
+                if (preg_match('/\d{3}/', $value)) {
+                    $values['extension_id'] = " (select extension_id from extensions where extension = '$value') ";
+                    $values['group_id'] = 'null';
+                } else {
+                    $values['group_id'] = " (select group_id from groups where groups.group = '$value') ";
+                    $values['extension_id'] = 'null';
+                }
+                break;
+            case 'destination':
+                if (preg_match('/\d{2,3}/', $value)) {
+                    $values[$key] = "'$value'";
+                }
+                else
+                    $values[$key] = "'$source[$value]'";
+                break;
+            default:
+                $values[$key] = "'$value'";
         }
     $rows[] = $values;
 }
