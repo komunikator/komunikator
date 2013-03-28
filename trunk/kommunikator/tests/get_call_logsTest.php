cdf493087fc52ac9af7ab0d64b306d4dd38de00a
ale <>
2013-03-28 16:30:05 +0300

    tests add

--- trunk/kommunikator/tests/get_call_logsTest.php
+++ trunk/kommunikator/tests/get_call_logsTest.php
@@ -23,7 +23,7 @@ class get_call_logsTest extends PHPUnit_Framework_TestCase {
     public function testCallLogs($form_data) {
         $params = array('action' => 'get_call_logs', 'session' => file_get_contents(sys_get_temp_dir() . '/session'));
         $params += $form_data;
-        print_r("php data_.php \"" . addslashes(json_encode($params))."\"");
+        //print_r("php data_.php \"" . addslashes(json_encode($params))."\"");
         $out = shell_exec("php data_.php \"" . addslashes(json_encode($params))."\"");
         $res = json_decode($out);
         print_r(count($res->data));
