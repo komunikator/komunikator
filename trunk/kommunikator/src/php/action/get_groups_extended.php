1442ca459b76d8369972038b5090473a797634a8
sda <sda@SDA.digt.local>
2013-04-23 18:37:27 +0400

    Исправление BUG 305 (Исключить возможность ввода информации в колонку "Группа")

--- trunk/kommunikator/src/php/action/get_groups_extended.php
+++ trunk/kommunikator/src/php/action/get_groups_extended.php
@@ -5,7 +5,7 @@ if(!$_SESSION['user']) {
 $total =  compact_array(query_to_array("SELECT count(*) FROM groups"));
 if(!is_array($total["data"]))  echo out(array("success"=>false,"message"=>$total));
     
-$data =  compact_array(query_to_array("SELECT group_id as id, groups.group, description, extension FROM groups WHERE group_id!=1".get_sql_order_limit()));
+$data =  compact_array(query_to_array("SELECT group_id as id, groups.group, description, extension FROM groups".get_sql_order_limit()));
 //file_put_contents("test.txt","SELECT group_id as id, \"group\", description, extension FROM groups ORDER BY ".get_sql_order_limit());
 
 if(!is_array($data["data"]))  echo out(array("success"=>false,"message"=>$data));
