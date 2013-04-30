ddefc2dbe4177824b6a382b9364bd5ab9a7f3c88
sda <sda@SDA.digt.local>
2013-04-30 15:36:13 +0400

    Попытка №1 BUG #297 Отображение времени работы сервера в панели "Статус АТС"

--- trunk/kommunikator/src/js/app/module/Status_Grid.js
+++ trunk/kommunikator/src/js/app/module/Status_Grid.js
@@ -1,4 +1,4 @@
-app.grid_show =  function(storeId){
+app.grid_show = function(storeId) {
     var grid = Ext.getCmp(storeId+'_grid');
     if (grid) {
         grid.ownerCt.ownerCt.setActiveTab(grid.ownerCt.ownerCt.items.indexOf(grid.ownerCt));
@@ -9,41 +9,48 @@ app.grid_show =  function(storeId){
 };		
 
 Ext.define('app.module.Status_Grid', {
-    //title: 'day statistic',
-    extend : 'app.Grid',
-    border:false,
-    style:'padding:15px;',
-    status_grid : true,
-    grid_id : 'Status_Grid',	
-    store_cfg: {
-        autorefresh : true,
-        fields : ['name', 'value'],
-        storeId :'statistic'  
+    // title : 'day statistic',
+    extend       : 'app.Grid',
+    border       : false,
+    style        : 'padding : 15px',
+    status_grid  : true,
+    grid_id      : 'Status_Grid',
+    
+    store_cfg    : {
+        autorefresh  : true,
+        fields       : ['name', 'value'],
+        storeId      : 'statistic'  
     },
-    height:500,
-    hideHeaders: true,
-    columns : [
+    
+    height       : 500,
+    hideHeaders  : true,
+    
+    columns      : [
     {
-        width: 120
+        width : 120
     },
     { 
-        flex: 1//width: 50
+        flex : 1
+        // width : 50
     }],
+
     columns_renderer : function(value, metadata, record, rowIndex, colIndex, store) {
         if (colIndex == 0 || colIndex == 1)
             if (app.msg[value])
                 value = app.msg[value];
-        if (colIndex == 1){
-            if (record.data.name =='status'){
-                var color = (value==app.msg['online'])?'green':'red';
+        
+        if (colIndex == 1) {
+            if (record.data.name == 'status') {
+                var color = (value==app.msg['online']) ? 'green' : 'red';
                 return '<span style="color:'+color+';">'+value+'</span>';
             }
-            if (record.data.name =='cpu_use'){
-                var color = (parseFloat(value.replace(/^([\d\.]+)\s%$/,"$1"))<app.critical_cpu)?'green':'red';
+            if (record.data.name == 'cpu_use') {
+                var color = (parseFloat(value.replace(/^([\d\.]+)\s%$/,"$1"))<app.critical_cpu) ? 'green' : 'red';
                 return '<span style="color:'+color+';">'+value+'</span>';
             }
         };
-        if (colIndex == 0){
+        
+        if (colIndex == 0) {
             if (record.data.name =='day_total_calls'){
                 return '<a href="#" onclick="app.grid_show('+"'call_logs'"+');">'+value+'</a>';
             };
@@ -54,9 +61,11 @@ Ext.define('app.module.Status_Grid', {
                 return '<a href="#" onclick="app.grid_show('+"'gateways'"+');">'+value+'</a>';
             }
         }
-        return value;		
+        
+        return value;
     },
-    initComponent : function () {
+            
+    initComponent : function() {
         this.callParent(arguments); 
     }
 })
