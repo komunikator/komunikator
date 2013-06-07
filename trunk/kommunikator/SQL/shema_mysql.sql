4d18397446c57cf1357dcb466addc99d6a2f0928
sda <sda@SDA.digt.local>
2013-06-07 17:25:16 +0400

    поправлена БД (таблицы: groups, playlist)

--- trunk/kommunikator/SQL/shema_mysql.sql
+++ trunk/kommunikator/SQL/shema_mysql.sql
@@ -56,7 +56,7 @@ CREATE TABLE `call_logs` (
   `reason` text,
   `ended` tinyint(1) DEFAULT NULL,
   UNIQUE KEY `time_indx` (`time`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -120,7 +120,7 @@ CREATE TABLE `dial_plans` (
   `position_to_start_adding` int(11) DEFAULT NULL,
   `digits_to_add` text,
   PRIMARY KEY (`dial_plan_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -139,7 +139,7 @@ CREATE TABLE `dids` (
   `extension_id` int(11) DEFAULT NULL,
   `group_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`did_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -166,18 +166,8 @@ CREATE TABLE `extensions` (
   `login_attempts` int(11) DEFAULT NULL,
   PRIMARY KEY (`extension_id`),
   UNIQUE KEY `extension` (`extension`)
-) ENGINE=MyISAM AUTO_INCREMENT=134 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `extensions`
---
-
-LOCK TABLES `extensions` WRITE;
-/*!40000 ALTER TABLE `extensions` DISABLE KEYS */;
-INSERT INTO `extensions` VALUES (1,'101','101101',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,NULL,NULL),(2,'102','102102',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,NULL,NULL);
-/*!40000 ALTER TABLE `extensions` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `gateways`
@@ -217,7 +207,7 @@ CREATE TABLE `gateways` (
   `trusted` tinyint(1) DEFAULT NULL,
   `sig_trunk_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`gateway_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -232,7 +222,7 @@ CREATE TABLE `group_members` (
   `group_id` int(11) DEFAULT NULL,
   `extension_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`group_member_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -272,8 +262,34 @@ CREATE TABLE `groups` (
   PRIMARY KEY (`group_id`),
   UNIQUE KEY `group` (`group`),
   UNIQUE KEY `extension` (`extension`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+
+/* добавление группы - "по умолчанию" */
+
+LOCK TABLES `groups` WRITE;
+
+INSERT INTO `groups`
+(
+`group_id`,
+`group`,
+`description`,
+`extension`,
+`mintime`,
+`length`,
+`maxout`,
+`greeting`,
+`maxcall`,
+`prompt`,
+`detail`,
+`playlist_id`
+)
+VALUES
+(NULL, '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+UNLOCK TABLES;
+
 
 --
 -- Table structure for table `incoming_gateways`
@@ -288,7 +304,7 @@ CREATE TABLE `incoming_gateways` (
   `gateway_id` int(11) DEFAULT NULL,
   `ip` text,
   PRIMARY KEY (`incoming_gateway_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -305,7 +321,7 @@ CREATE TABLE `keys` (
   `destination` text,
   `description` text,
   PRIMARY KEY (`key_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -321,7 +337,7 @@ CREATE TABLE `limits_international` (
   `name` text,
   `value` text,
   PRIMARY KEY (`limit_international_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -337,7 +353,7 @@ CREATE TABLE `music_on_hold` (
   `description` text,
   `file` text,
   PRIMARY KEY (`music_on_hold_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=69 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -371,7 +387,7 @@ CREATE TABLE `ntn_settings` (
   `value` text,
   `description` text,
   PRIMARY KEY (`ntn_setting_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -387,7 +403,7 @@ CREATE TABLE `pbx_settings` (
   `param` text,
   `value` text,
   PRIMARY KEY (`pbx_setting_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -402,7 +418,7 @@ CREATE TABLE `playlist_items` (
   `playlist_id` int(11) DEFAULT NULL,
   `music_on_hold_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`playlist_item_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -417,8 +433,21 @@ CREATE TABLE `playlists` (
   `playlist` text,
   `in_use` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`playlist_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+
+/* добавление плейлиста - по умолчанию */
+
+INSERT INTO `playlists`
+(
+`playlist_id`,
+`playlist`,
+`in_use`
+)
+VALUES
+(NULL, '', '0');
+
 
 --
 -- Table structure for table `prefixes`
@@ -433,7 +462,7 @@ CREATE TABLE `prefixes` (
   `name` text,
   `international` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`prefix_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -450,7 +479,7 @@ CREATE TABLE `prompts` (
   `status` text,
   `file` text,
   PRIMARY KEY (`prompt_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -466,18 +495,8 @@ CREATE TABLE `settings` (
   `value` text,
   `description` text,
   PRIMARY KEY (`setting_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=261 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `settings`
---
-
-LOCK TABLES `settings` WRITE;
-/*!40000 ALTER TABLE `settings` DISABLE KEYS */;
-INSERT INTO `settings` VALUES (1,'vm','external/nodata/leavemaildb.php','Script used for leaving a voicemail message.'),(2,'version','1',NULL),(3,'annonymous_calls','no','Allow calls from anomynous users if call is for one of the extensions. Use just \'yes\' or \'no\' as values.'),(5,'callername',NULL,NULL),(6,'prefix',NULL,NULL),(4,'callerid','79278837050',NULL),(7,'international_calls','yes','Allow calls to international/expensive destinations. This prefixes are set in Outbound>>International calls'),(8,'international_calls_live','no','Disabled at 2012-12-18 12:04:18.885601+04., limit 1minute');
-/*!40000 ALTER TABLE `settings` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `short_names`
@@ -493,7 +512,7 @@ CREATE TABLE `short_names` (
   `number` text,
   `extension_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`short_name_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -548,14 +567,34 @@ CREATE TABLE `time_frames` (
   `end_hour` text,
   `numeric_day` int(11) DEFAULT NULL,
   PRIMARY KEY (`time_frame_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+
+/* настройка расписания рабочего времени */
+
 LOCK TABLES `time_frames` WRITE;
-/*!40000 ALTER TABLE `time_frames` DISABLE KEYS */;
-INSERT INTO `time_frames` VALUES (1,1,'Sunday',NULL,NULL,0),(2,1,'Monday','4','14',1),(3,1,'Tuesday','4','14',2),(4,1,'Wednesday','4','14',3),(5,1,'Thursday','4','14',4),(6,1,'Friday','4','13',5),(7,1,'Saturday',NULL,NULL,6);
-/*!40000 ALTER TABLE `time_frames` ENABLE KEYS */;
+
+INSERT INTO `time_frames`
+(
+`time_frame_id`,
+`prompt_id`,
+`day`,
+`start_hour`,
+`end_hour`,
+`numeric_day`
+)
+VALUES
+(NULL, '1', 'Sunday',     NULL, NULL, '0'),
+(NULL, '1', 'Monday',     '4',  '14', '1'),
+(NULL, '1', 'Tuesday',    '4',  '14', '2'),
+(NULL, '1', 'Wednesday',  '4',  '14', '3'),
+(NULL, '1', 'Thursday',   '4',  '14', '4'),
+(NULL, '1', 'Friday',     '4',  '14', '5'),
+(NULL, '1', 'Saturday',   NULL, NULL, '6');
+
 UNLOCK TABLES;
+
 
 --
 -- Table structure for table `users`
@@ -576,26 +615,28 @@ CREATE TABLE `users` (
   `ident` text,
   `login_attempts` int(11) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `users`
---
+
+/* создание учетной записи - администратор */
 
 LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin','admin',NULL,NULL,NULL,NULL,NULL,NULL,0);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+INSERT INTO `users`
+(
+`user_id`,
+`username`,
+`password`,
+`firstname`,
+`lastname`,
+`email`,
+`description`,
+`fax_number`,
+`ident`,
+`login_attempts`
+)
+VALUES
+(NULL, 'admin', 'admin', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-03-06 23:24:00
