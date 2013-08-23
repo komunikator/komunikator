b3ac3873a9eac5617f3e3f90f5333f1fd9e7d5a4
eprR <eprR@172.17.2.47>
2013-08-23 09:06:43 +0400

    переадресация

--- trunk/kommunikator/src/js/app/module/Forwarding_Panel.js
+++ trunk/kommunikator/src/js/app/module/Forwarding_Panel.js
@@ -79,7 +79,7 @@ Ext.define('app.module.Forwarding_Panel', {
                         height: 20,
                         editor: {
                             xtype: 'numberfield',
-                            minValue: 1
+                            minValue: 0
                         }
                     },
                     {
