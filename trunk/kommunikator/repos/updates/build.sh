2c3b5ae794350841b8b011833c6713ee76d5df60
alexander.elkin <1@172.17.2.22>
2013-08-29 12:19:51 +0400

    добавлено тестовое обновление

--- trunk/kommunikator/repos/updates/build.sh
+++ trunk/kommunikator/repos/updates/build.sh
@@ -4,7 +4,7 @@ ver=05001
 build_path="./build" 
 p=`pwd`
 cd $ver
-tar --exclude-caches -zcvf $p/$name_$ver.tar.gz .
+tar --exclude-caches -zcvf $p/$name.$ver.tar.gz .
 cd $p
-cp $name_$ver.tar.gz $build_path
-rm $name_$ver.tar.gz
+cp $name.$ver.tar.gz $build_path
+rm $name.$ver.tar.gz
