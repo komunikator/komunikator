ef806cce5c52ec2ab9a4ea2007435cb2159818e5
sda <sda@SDA.digt.local>
2013-12-06 18:27:56 +0400

    create_call_button

--- trunk/kommunikator/src/php/action/create_sqlite.php
+++ trunk/kommunikator/src/php/action/create_sqlite.php
@@ -59,9 +59,6 @@ $table_name = get_sql_field( next( explode('_', $action, 2) ) );
 foreach ($rows as $row) {
     $sql = sprintf( 'INSERT INTO %s (%s) VALUES (%s)', $table_name, implode(', ', array_map( $conn->escapeSimple, array_map( 'get_sql_field', array_keys($row) ) ) ), implode(', ', array_map( $conn->escapeSimple, $row) ) );
     query($sql);
-    
-    $sql = "INSERT INTO actionlogs (date, performer, query, ip) VALUES (".time().", \"{$_SESSION['user']}\", \"$sql\", \"{$_SERVER['REMOTE_ADDR']}\")";
-    query($sql);
 };
 
 if (!isset($need_out)) {
