d7acf53f692cc698cc3472eea67b4b169424097b
sda <sda@SDA.digt.local>
2013-12-19 18:21:53 +0400

    Правка кода (панель Звонок с сайта)

--- trunk/kommunikator/scripts/config.php
+++ trunk/kommunikator/scripts/config.php
@@ -80,9 +80,9 @@ require_once 'PEAR.php';
 if ( in_array( $action, array('get_call_button', 'create_call_button', 'create_account', 'create_account_sip', 'create_account_sip_caller', 'destroy_call_button', 'update_call_button', 'update_account', 'update_account_sip', 'update_account_sip_caller', 'get_button_code') ) ) {
     $db_type_sql = "sqlite3";
     
-    // $db_sqlite3_path = "/etc/webrtc2sip/c2c_sqlite.db";  // абсолютный путь к файлу БД
+    $db_sqlite3_path = "/etc/webrtc2sip/c2c_sqlite.db";  // абсолютный путь к файлу БД
     
-    $db_sqlite3_path = "/var/www/sda/sqlite.ru/c2c_sqlite.db";
+    // $db_sqlite3_path = "/var/www/sda/sqlite/c2c_sqlite.db";
     
     
     // - - - - - - - - - - - - - - - - - - - -
