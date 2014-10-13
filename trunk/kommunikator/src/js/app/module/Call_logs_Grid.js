0c14e9d06616180f2eab6d34d98a9765a8138996
eprR <eprR@172.17.2.47>
2014-10-13 11:46:11 +0400

    history optimization

--- trunk/kommunikator/src/js/app/module/Call_logs_Grid.js
+++ trunk/kommunikator/src/js/app/module/Call_logs_Grid.js
@@ -83,7 +83,7 @@ Ext.define('app.module.Call_logs_Grid', {
                 type: 'date',
                 dateFormat: app.date_format
             }, 'type', 'caller', 'called', 'duration', 'gateway', 'status', 'record'],
-        storeId: 'call_logs',
+        storeId: 'call_history',
         sorters: [{
                 direction: 'DESC',
                 property: 'time'
