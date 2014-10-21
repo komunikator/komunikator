73a1c14991ce66b34b8fe26785b0ee8f66d6b6b1
eprR <eprR@172.17.2.47>
2014-10-21 16:59:22 +0400

    call_history - добавлено условие сбора имени файла

--- trunk/kommunikator/src/php/action/get_call_history.php
+++ trunk/kommunikator/src/php/action/get_call_history.php
@@ -97,7 +97,11 @@ SELECT * FROM (
             ELSE NULL
         END gateway,
         a.status,
-        record       
+        CASE
+            WHEN time IS NOT NULL 
+                THEN CONCAT( time, '_',caller, '_', called)
+            ELSE NULL
+        END record        
     FROM call_history a
     LEFT JOIN extensions x1 ON x1.extension = caller
     LEFT JOIN extensions x2 ON x2.extension = called
