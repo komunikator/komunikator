e3b78de82468a7d3214bb0b0e8bf5503c9a40391
eprR <eprR@172.17.2.47>
2014-11-20 13:00:35 +0400

    geoinfo

--- trunk/kommunikator/src/js/app/module/Call_logs_Grid.js
+++ trunk/kommunikator/src/js/app/module/Call_logs_Grid.js
@@ -170,8 +170,9 @@ Ext.define('app.module.Call_logs_Grid', {
                     var obj = JSON.parse(value);
                     for (var key in obj) {
                         for (var key1 in obj[key]) {
-                            Ext.call_site_hint = Ext.call_site_hint + app.msg[key1] + " : " + obj[key][key1] + "<br/>";
-                            call_site_params.push([key1, obj[key][key1]]); 
+                            var val = app.msg[key1] ? app.msg[key1] : key1;
+                            Ext.call_site_hint = Ext.call_site_hint + val + " : " + obj[key][key1] + "<br/>";
+                            call_site_params.push([key1, obj[key][key1]]);
                         }
                     }
                     metadata.tdAttr = 'data-qtip="' + Ext.call_site_hint + '"';//выводим подсказку при наведении 
