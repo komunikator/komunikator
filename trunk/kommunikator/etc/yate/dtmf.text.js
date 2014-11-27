faf95014e716a1b998d6d69c3073e9c9e3ac1a61
ale <alexander.elkin@digt.ru>
2014-11-27 17:14:30 +0300

    add GEO INFO

--- trunk/kommunikator/etc/yate/dtmf.text.js
+++ trunk/kommunikator/etc/yate/dtmf.text.js
@@ -1,3 +1,18 @@
+//TODO
+//add to c2c-api.js 
+//GEO INFO
+// var about = {}; 
+// function geoData(data){about = {geo:data};}; 
+// document.write(unescape("%3Cscript src='http://www.telize.com/geoip?callback=geoData' + (new Date).getTime(); type='application/javascript'%3E%3C/script%3E"));
+// ...
+//        tsk_utils_log_info('[C2C] session event = ' + e.type);
+//        switch (e.type) {
+//+++	    case 'connected': c2c.callSession.dtmf(escape(JSON.stringify(about)));//dtmf hack
+//            case 'connecting': case 'connected':
+// ...
+//GEO INFO
+
+
 function onText(msg)
 {
     //Engine.print_r(msg);
