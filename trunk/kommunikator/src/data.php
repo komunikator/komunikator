f6f6dea2eb5be1ed23c18de5988d70831614c127
eprR <eprR@172.17.2.47>
2015-01-29 15:35:58 +0400

    call order

--- trunk/kommunikator/src/data.php
+++ trunk/kommunikator/src/data.php
@@ -1,58 +1,57 @@
 <?php
 
 /*
-*  | RUS | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ *  | RUS | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-*    «Komunikator» – Web-интерфейс для настройки и управления программной IP-АТС «YATE»
-*    Copyright (C) 2012-2013, ООО «Телефонные системы»
+ *    «Komunikator» – Web-интерфейс для настройки и управления программной IP-АТС «YATE»
+ *    Copyright (C) 2012-2013, ООО «Телефонные системы»
 
-*    ЭТОТ ФАЙЛ является частью проекта «Komunikator»
+ *    ЭТОТ ФАЙЛ является частью проекта «Komunikator»
 
-*    Сайт проекта «Komunikator»: http://4yate.ru/
-*    Служба технической поддержки проекта «Komunikator»: E-mail: support@4yate.ru
+ *    Сайт проекта «Komunikator»: http://4yate.ru/
+ *    Служба технической поддержки проекта «Komunikator»: E-mail: support@4yate.ru
 
-*    В проекте «Komunikator» используются:
-*      исходные коды проекта «YATE», http://yate.null.ro/pmwiki/
-*      исходные коды проекта «FREESENTRAL», http://www.freesentral.com/
-*      библиотеки проекта «Sencha Ext JS», http://www.sencha.com/products/extjs
+ *    В проекте «Komunikator» используются:
+ *      исходные коды проекта «YATE», http://yate.null.ro/pmwiki/
+ *      исходные коды проекта «FREESENTRAL», http://www.freesentral.com/
+ *      библиотеки проекта «Sencha Ext JS», http://www.sencha.com/products/extjs
 
-*    Web-приложение «Komunikator» является свободным и открытым программным обеспечением. Тем самым
-*  давая пользователю право на распространение и (или) модификацию данного Web-приложения (а также
-*  и иные права) согласно условиям GNU General Public License, опубликованной
-*  Free Software Foundation, версии 3.
+ *    Web-приложение «Komunikator» является свободным и открытым программным обеспечением. Тем самым
+ *  давая пользователю право на распространение и (или) модификацию данного Web-приложения (а также
+ *  и иные права) согласно условиям GNU General Public License, опубликованной
+ *  Free Software Foundation, версии 3.
 
-*    В случае отсутствия файла «License» (идущего вместе с исходными кодами программного обеспечения)
-*  описывающего условия GNU General Public License версии 3, можно посетить официальный сайт
-*  http://www.gnu.org/licenses/ , где опубликованы условия GNU General Public License
-*  различных версий (в том числе и версии 3).
+ *    В случае отсутствия файла «License» (идущего вместе с исходными кодами программного обеспечения)
+ *  описывающего условия GNU General Public License версии 3, можно посетить официальный сайт
+ *  http://www.gnu.org/licenses/ , где опубликованы условия GNU General Public License
+ *  различных версий (в том числе и версии 3).
 
-*  | ENG | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ *  | ENG | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-*    "Komunikator" is a web interface for IP-PBX "YATE" configuration and management
-*    Copyright (C) 2012-2013, "Telephonnyie sistemy" Ltd.
+ *    "Komunikator" is a web interface for IP-PBX "YATE" configuration and management
+ *    Copyright (C) 2012-2013, "Telephonnyie sistemy" Ltd.
 
-*    THIS FILE is an integral part of the project "Komunikator"
+ *    THIS FILE is an integral part of the project "Komunikator"
 
-*    "Komunikator" project site: http://4yate.ru/
-*    "Komunikator" technical support e-mail: support@4yate.ru
+ *    "Komunikator" project site: http://4yate.ru/
+ *    "Komunikator" technical support e-mail: support@4yate.ru
 
-*    The project "Komunikator" are used:
-*      the source code of "YATE" project, http://yate.null.ro/pmwiki/
-*      the source code of "FREESENTRAL" project, http://www.freesentral.com/
-*      "Sencha Ext JS" project libraries, http://www.sencha.com/products/extjs
+ *    The project "Komunikator" are used:
+ *      the source code of "YATE" project, http://yate.null.ro/pmwiki/
+ *      the source code of "FREESENTRAL" project, http://www.freesentral.com/
+ *      "Sencha Ext JS" project libraries, http://www.sencha.com/products/extjs
 
-*    "Komunikator" web application is a free/libre and open-source software. Therefore it grants user rights
-*  for distribution and (or) modification (including other rights) of this programming solution according
-*  to GNU General Public License terms and conditions published by Free Software Foundation in version 3.
+ *    "Komunikator" web application is a free/libre and open-source software. Therefore it grants user rights
+ *  for distribution and (or) modification (including other rights) of this programming solution according
+ *  to GNU General Public License terms and conditions published by Free Software Foundation in version 3.
 
-*    In case the file "License" that describes GNU General Public License terms and conditions,
-*  version 3, is missing (initially goes with software source code), you can visit the official site
-*  http://www.gnu.org/licenses/ and find terms specified in appropriate GNU General Public License
-*  version (version 3 as well).
+ *    In case the file "License" that describes GNU General Public License terms and conditions,
+ *  version 3, is missing (initially goes with software source code), you can visit the official site
+ *  http://www.gnu.org/licenses/ and find terms specified in appropriate GNU General Public License
+ *  version (version 3 as well).
 
-*  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-*/
-
+ *  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ */
 ?><?php
 
 //ini_set('session.save_path', $_SERVER['DOCUMENT_ROOT'] .'/tmp/');
@@ -69,8 +68,8 @@ session_start();
 //ini_set("display_errors",true);
 
 $log_file = 'log.log';
-ini_set("log_errors",true);
-ini_set("error_log",$log_file);
+ini_set("log_errors", true);
+ini_set("error_log", $log_file);
 
 
 // - - - - - - - - - - - - - - - - - - - -
@@ -85,43 +84,44 @@ $action = getparam("action");
 require_once("libyate.php");
 require_once("lib_queries.php");
 
-$date_format = "d.m.y H:i:s"; 
+$date_format = "d.m.y H:i:s";
 
-if (!$conn) die('Database Connection Failed');
+if (!$conn)
+    die('Database Connection Failed');
 
 function handle_pear_error2($e) {
-    die($e->getMessage().' '.print_r($e->getUserInfo(),true));
+    die($e->getMessage() . ' ' . print_r($e->getUserInfo(), true));
 //echo json_encode(array("success"=>false,"message"=>"Error:'".$e->getMessage()/*.' '.print_r($e->getUserInfo(),true)*/));
-};
+}
+
+;
 
 require_once 'PEAR.php';
 PEAR::setErrorHandling(PEAR_ERROR_CALLBACK, 'handle_pear_error2');
 
 // ini_set("session.use_only_cookies", "0");
-
-
 // - - - - - - - - - - - - - - - - - - - -
-
 // переместили на вверх
 // до загрузки файла config.php (который, загружается из файла lib_queries.php)
-
 // require_once "php/util.php";
-
 // $action = getparam("action");
-
 // - - - - - - - - - - - - - - - - - - - -
 
-
-if ($action!='auth' && !(isset($_SESSION['user']) || isset($_SESSION['extension'])) || $action!='order_call') {
-    echo out(array("success"=>false,"message"=>"session_failed"));
-    exit;
+if ($action != 'order_call') {
+    if ($action != 'auth' && !( isset($_SESSION['user']) || isset($_SESSION['extension']))) {
+        echo out(array("success" => false, "message" => "session_failed"));
+        exit;
+    }
 }
 
-if (!isset($_SESSION['time_offset'])) $_SESSION['time_offset'] = 240;
-$action_path = "php/action"; 
+if (!isset($_SESSION['time_offset'])
+)
+    $_SESSION['time_offset'] = 240;
+$action_path = "php/action";
 
-if (file_exists("$action_path/$action".".php")) include "$action_path/$action".".php"; 
-else 
-   echo out(array("success"=>false,"message"=>"Unknown action '".$action."'"));
-
+if (file_exists("$action_path/$action" . ".php")
+)
+    include "$action_path/$action" . ".php";
+else
+    echo out(array("success" => false, "message" => "Unknown action '" . $action . "'"));
 ?>
