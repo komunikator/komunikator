f1a47eb506b1739ccbbdd3dee1d312ea557616a3
eprR <eprR@172.17.2.47>
2015-02-25 12:07:41 +0400

    корректировка кода

--- trunk/kommunikator/src/js/app/locale/ru.js
+++ trunk/kommunikator/src/js/app/locale/ru.js
@@ -6,8 +6,8 @@
  
  *    ЭТОТ ФАЙЛ является частью проекта «Komunikator»
  
- *    Сайт проекта «Komunikator»: http://4yate.ru/
- *    Служба технической поддержки проекта «Komunikator»: E-mail: support@4yate.ru
+ *    Сайт проекта «Komunikator»: http://komunikator.ru/
+ *    Служба технической поддержки проекта «Komunikator»: E-mail: support@komunikator.ru
  
  *    В проекте «Komunikator» используются:
  *      исходные коды проекта «YATE», http://yate.null.ro/pmwiki/
@@ -31,8 +31,8 @@
  
  *    THIS FILE is an integral part of the project "Komunikator"
  
- *    "Komunikator" project site: http://4yate.ru/
- *    "Komunikator" technical support e-mail: support@4yate.ru
+ *    "Komunikator" project site: http://komunikator.ru/
+ *    "Komunikator" technical support e-mail: support@komunikator.ru
  
  *    The project "Komunikator" are used:
  *      the source code of "YATE" project, http://yate.null.ro/pmwiki/
@@ -252,7 +252,6 @@ app.msg = {
     Mail_Settings_Panel: 'Почтовые уведомления', //2--//--
     Call_Record_Grid: 'Запись звонка', //3--//--
     Call_back_Grid: 'Перезвоните мне',
-
     //-------предупреждения
     session_failed: 'Время сеанса истекло. Пожалуйста, войдите еще раз',
     fail_load: 'Сбой загрузки',
@@ -379,7 +378,7 @@ app.msg = {
     region: 'Регион',
     timezone: 'Часовой пояс',
     continent_code: 'Код континента',
-    dma_code: 'Код рыночной зоны',/*'Код рыночной зоны (DMA, только для США и Канады)',*/
+    dma_code: 'Код рыночной зоны', /*'Код рыночной зоны (DMA, только для США и Канады)',*/
     area_code: 'Код телефонной сети общего пользования',
     isp: 'Имя провайдера(ISP) указаного IP адреса',
     asn: 'Номера АС',
--- trunk/kommunikator/src/js/app/module/Call_Record_Grid.js
+++ trunk/kommunikator/src/js/app/module/Call_Record_Grid.js
@@ -1,3 +1,56 @@
+/*
+ *  | RUS | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ 
+ *    «Komunikator» – Web-интерфейс для настройки и управления программной IP-АТС «YATE»
+ *    Copyright (C) 2012-2013, ООО «Телефонные системы»
+ 
+ *    ЭТОТ ФАЙЛ является частью проекта «Komunikator»
+ 
+ *    Сайт проекта «Komunikator»: http://komunikator.ru/
+ *    Служба технической поддержки проекта «Komunikator»: E-mail: support@komunikator.ru
+ 
+ *    В проекте «Komunikator» используются:
+ *      исходные коды проекта «YATE», http://yate.null.ro/pmwiki/
+ *      исходные коды проекта «FREESENTRAL», http://www.freesentral.com/
+ *      библиотеки проекта «Sencha Ext JS», http://www.sencha.com/products/extjs
+ 
+ *    Web-приложение «Komunikator» является свободным и открытым программным обеспечением. Тем самым
+ *  давая пользователю право на распространение и (или) модификацию данного Web-приложения (а также
+ *  и иные права) согласно условиям GNU General Public License, опубликованной
+ *  Free Software Foundation, версии 3.
+ 
+ *    В случае отсутствия файла «License» (идущего вместе с исходными кодами программного обеспечения)
+ *  описывающего условия GNU General Public License версии 3, можно посетить официальный сайт
+ *  http://www.gnu.org/licenses/ , где опубликованы условия GNU General Public License
+ *  различных версий (в том числе и версии 3).
+ 
+ *  | ENG | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ 
+ *    "Komunikator" is a web interface for IP-PBX "YATE" configuration and management
+ *    Copyright (C) 2012-2013, "Telephonnyie sistemy" Ltd.
+ 
+ *    THIS FILE is an integral part of the project "Komunikator"
+ 
+ *    "Komunikator" project site: http://komunikator.ru/
+ *    "Komunikator" technical support e-mail: support@komunikator.ru
+ 
+ *    The project "Komunikator" are used:
+ *      the source code of "YATE" project, http://yate.null.ro/pmwiki/
+ *      the source code of "FREESENTRAL" project, http://www.freesentral.com/
+ *      "Sencha Ext JS" project libraries, http://www.sencha.com/products/extjs
+ 
+ *    "Komunikator" web application is a free/libre and open-source software. Therefore it grants user rights
+ *  for distribution and (or) modification (including other rights) of this programming solution according
+ *  to GNU General Public License terms and conditions published by Free Software Foundation in version 3.
+ 
+ *    In case the file "License" that describes GNU General Public License terms and conditions,
+ *  version 3, is missing (initially goes with software source code), you can visit the official site
+ *  http://www.gnu.org/licenses/ and find terms specified in appropriate GNU General Public License
+ *  version (version 3 as well).
+ 
+ *  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ */
+
 var type = Ext.create('Ext.data.Store', {
     fields: ['id', 'type'],
     data: [
@@ -69,10 +122,7 @@ Ext.define('app.module.Call_Record_Grid', {
 
                     },
                     renderer: function(v) {
-                        //var t = '';
                         return  (v == '*') ? app.msg.All : v;
-                        //if(v == '*'){t =app.msg.All; } else{t = v;}
-                        //if(v == null)
                     }
                 },
                 {// 'group'
@@ -257,7 +307,8 @@ Ext.define('app.module.Call_Record_Grid', {
                                 ),
                         listeners: {
                             change:
-                                    function(f, new_val) {console.log(f.ownerCt.items.items[7].value);
+                                    function(f, new_val) {
+                                        console.log(f.ownerCt.items.items[7].value);
                                         if (new_val == '' && f.ownerCt.items.items[7].value == null) {
                                             f.ownerCt.items.items[6].setValue('*');
                                         }
@@ -329,7 +380,6 @@ Ext.define('app.module.Call_Record_Grid', {
                 xtype: 'textfield'
             }
         }
-
     ],
     viewConfig: {
         stripeRows: true
