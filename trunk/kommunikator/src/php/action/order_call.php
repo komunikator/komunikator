23b87c69b44076d9a018937da215bf4e27b39af0
eprR <eprR@172.17.2.47>
2015-03-04 16:59:47 +0400

    звонок на группу

--- trunk/kommunikator/src/php/action/order_call.php
+++ trunk/kommunikator/src/php/action/order_call.php
@@ -54,18 +54,6 @@
  */
 ?><?
 
-//$caller = $_GET["caller"];//добавить проверку
-//$site = $_GET["site"];//добавить проверк
-//получаем Номер исполняющий заказ звонка. Если он отсутствует - выводим ошибку
-/* $obj = "SELECT value FROM additional_settings WHERE settings_id=3 AND description = 'call_order_executor'";
-  $caller = query_to_array($obj);
-  if ($caller[0]['value'] == '' || $caller[0]['value'] == null) {
-  $message = "Call order executor is undefined";
-  $jsonResponse = "{\"warning\":\"" . $message . "\"}";
-  echo $callback . "(" . $jsonResponse . ")";
-  exit;
-  } */
-//-----
 require_once("php/socketconn.php");
 
 header("Content-Type: application/javascript");
@@ -73,15 +61,12 @@ $callback = $_GET["callback"];
 $called = getparam("number");
 $call_back_id = $_GET["call_back_id"];
 
+
 $sql1 = "SELECT destination, name_site, callthrough_time FROM call_back WHERE call_back_id = $call_back_id";
 $res = query_to_array($sql1);
-$caller = $res[0]["destination"];
-$site = urlencode($res[0]["name_site"]);
-$callthrough_time = $res[0]["callthrough_time"];
 
-//echo("----------------------------------------------". $site);
-//--------
-//если номер телефона, на который нужно совершить заказанный звонок, не передан - выводим ошибку
+// - - - - - - - error checking - - - - - - -
+
 $check_called = str_replace(' ', '', $called);
 if (!$called || $check_called == '') {
     $message = "Phone number is undefined";
@@ -90,40 +75,33 @@ if (!$called || $check_called == '') {
     exit;
 }
 
-//если с одного и того же браузера(или одна и та же сессия) получаем запрос несколько раз подряд 
-//запрос на заказ звонка, то проверяем промежуток между этим запросами. Он должен быть не менее 30сек
+if (!$call_back_id || $call_back_id == '' || count($res) == 0) {
+    $message = "Caller undefined";
+    $jsonResponse = "{\"warning\":\"" . $message . "\"}";
+    echo $callback . "(" . $jsonResponse . ")";
+    exit;
+}
+
+$caller = $res[0]["destination"];
+$site = urlencode($res[0]["name_site"]);
+$callthrough_time = $res[0]["callthrough_time"];
+
 if ($_SESSION['last_action']) {
-    if ((time() - $_SESSION['last_action']) < 30) {
-        $message = "You can reorder the call after 30 seconds";
+    if ((time() - $_SESSION['last_action']) < ($callthrough_time + 5)) {
+        $message = "You can reorder the call after " . ($callthrough_time + 5) . " seconds";
         $jsonResponse = "{\"warning\":\"" . $message . "\"}";
         echo $callback . "(" . $jsonResponse . ")";
         exit;
     }
 }
 
-//записываем время сессии и после этого закрываем файл, где пишутся некие данные о сессии
-//это сделано, чтобы можно было с одной сессии делать один и тот же запрос несколько раз
-//(иначе, пока первый запрос не выполнится, второй также не выполняется. с закрыванием - 
-//можно выполнять второй запрос, пока первый еще не завершился)
+// - - - - - - - error checking(END) - - - - - - -
+
 $_SESSION['last_action'] = time();
 session_write_close();
 
-$command = "click_to_call $caller $called $site $callthrough_time";
-
-$socket = new SocketConn;
-if ($socket->error == "") {
-    $obj = array("success" => true);
-    $socket->command($command);
-    $sql = "insert into actionlogs (date,performer,log,ip) values (" . time() . ",\"{$_SESSION['extension']}\",\"$command\", \"{$_SERVER['REMOTE_ADDR']}\")";
-    query($sql);
-} else {
-    $obj = array("success" => false);
-    $obj['message'] = "Can't make call. Please contact your system administrator.";
-    echo out($obj);
-}
-
 //проверяем есть ли активные звонки с нужными параметрами
-$sql =
+$sql_action_call =
         <<<EOD
 select * from (
 select
@@ -148,23 +126,120 @@ from call_logs a
     (a.status="answered" and b.status="answered")) a
 EOD;
 
-$count = round($res[0]["callthrough_time"] / 4, 0, PHP_ROUND_HALF_DOWN);
-for ($i = 0; $i < $count; $i++) {
-    sleep(4);
-    $data = compact_array(query_to_array($sql));
-    $total = count($data["data"]);
-    // соединение прошло успешно, отвечаем true
-    if ($total > 0) {
-        $message = "true";
-        $jsonResponse = "{\"success\":\"" . $message . "\"}";
-        echo $callback . "(" . $jsonResponse . ")";
-        break;
-    }
-    //если соединение между АБОНЕНТАМИ не произошло в течении 25сек, отвечаем false
-    if ($i == ($count - 1) && $total == 0) {
-        $message = "false";
-        $jsonResponse = "{\"success\":\"" . $message . "\"}";
-        echo $callback . "(" . $jsonResponse . $count . ")";
+function click_to_call($caller, $called, $site, $callthrough_time) {
+    $command = "click_to_call $caller $called $site $callthrough_time";
+
+    $socket = new SocketConn;
+    if ($socket->error == "") {
+        $obj = array("success" => true);
+        $socket->command($command);
+        $sql = "insert into actionlogs (date,performer,log,ip) values (" . time() . ",\"{$_SESSION['extension']}\",\"$command\", \"{$_SERVER['REMOTE_ADDR']}\")";
+        query($sql);
+    } else {
+        $obj = array("success" => false);
+        $obj['message'] = "Can't make call. Please contact your system administrator.";
+        echo out($obj);
     }
 }
-?>
+
+if (strlen($caller) == 2) {
+    $sql = "SELECT group_id FROM groups WHERE extension = $caller";
+    $res = query_to_array($sql);
+    if ($res) {
+        $group_id = $res[0]["group_id"];
+        $last_priority = NULL;
+
+        $sql1 = "
+                        SELECT e.extension as number, gp.priority
+            FROM 
+                (SELECT extensions.extension_id, extension, inuse_last 
+                 FROM extensions,group_members 
+                 WHERE extensions.extension_id=group_members.extension_id
+                 AND group_members.group_id=$group_id
+                 AND coalesce(extensions.inuse_count,0)=0
+                 AND extensions.expires is not NULL) e 
+            LEFT JOIN group_priority gp ON e.extension_id = gp.extension_id and gp.group_id = $group_id
+     $last_priority 
+
+	    ORDER BY priority DESC, inuse_last";
+
+
+        $count = round($callthrough_time / 11, 0, PHP_ROUND_HALF_DOWN);
+
+        for ($i = 0; $i < $count; $i++) {
+            $res1 = query_to_array($sql1);
+            $caller = $res1[0]["number"];
+            if (!$res1 && $last_priority == NULL) {
+                $message = "Caller undefined";
+                $jsonResponse = "{\"warning\":\"" . $message . "\"}";
+                echo $callback . "(" . $jsonResponse . ")";
+                exit;
+            }
+            if (!$res1 && $last_priority !== NULL) {
+                $last_priority = NULL;
+                $res1 = query_to_array($sql1);
+                if (!$res1) {
+                    $message = "false";
+                    $jsonResponse = "{\"success\":\"" . $message . "\"}";
+                    echo $callback . "(" . $jsonResponse . ")";
+                    break;
+                }
+                ;
+            }
+            click_to_call($caller, $called, $site, 10);
+
+            for ($k = 0; $k < 3; $k++) {
+                sleep(3);
+
+                $data = compact_array(query_to_array($sql_action_call));
+                $total = count($data["data"]);
+
+                if ($total > 0) {
+                    $stop = true;
+                    break;
+                }
+            }
+            if ($stop) {
+                $message = "true";
+                $jsonResponse = "{\"success\":\"" . $message . "\"}";
+                echo $callback . "(" . $jsonResponse . ")";
+                break;
+            }
+            if ($i == ($count - 1) && !$stop) {
+                $message = "false";
+                $jsonResponse = "{\"success\":\"" . $message . "\"}";
+                echo $callback . "(" . $jsonResponse . ")";
+            }
+            $last_priority = "WHERE gp.priority < " . $res1[0]["priority"];
+            sleep(2);
+        }
+    } else {
+        $message = "Caller group undefined";
+        $jsonResponse = "{\"warning\":\"" . $message . "\"}";
+        echo $callback . "(" . $jsonResponse . ")";
+        exit;
+    }
+} else {
+
+    click_to_call($caller, $called, $site, $callthrough_time);
+
+    $count = round($callthrough_time / 4, 0, PHP_ROUND_HALF_DOWN);
+    for ($i = 0; $i < $count; $i++) {
+        sleep(4);
+        $data = compact_array(query_to_array($sql_action_call));
+        $total = count($data["data"]);
+        // соединение прошло успешно, отвечаем true
+        if ($total > 0) {
+            $message = "true";
+            $jsonResponse = "{\"success\":\"" . $message . "\"}";
+            echo $callback . "(" . $jsonResponse . ")";
+            break;
+        }
+        //если соединение между АБОНЕНТАМИ не произошло в течении 25сек, отвечаем false
+        if ($i == ($count - 1) && $total == 0) {
+            $message = "false";
+            $jsonResponse = "{\"success\":\"" . $message . "\"}";
+            echo $callback . "(" . $jsonResponse . ")";
+        }
+    }
+}
